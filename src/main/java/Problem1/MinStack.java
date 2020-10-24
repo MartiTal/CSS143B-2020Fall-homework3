90e0b778117e4829e7c9f258a16e8e923f46cdde
TalonM <talonm@uw.edu>
2020-10-23 21:16:55 -0700

    testValidParentheses implemented and passes

--- src/main/java/Problem1/MinStack.java
+++ src/main/java/Problem1/MinStack.java
@@ -4,10 +4,12 @@ public class MinStack extends ArrayStack<Integer> {
     // okay to add member variables
     // can only use Stack interface and ArrayStack from this folder
     // do not use Java Stack
+    private ArrayStack<Integer> minis;
 
     public MinStack(int size) {
         // homework
         super(size); // place holder
+        minis = new ArrayStack<Integer>(size);
     }
 
     @Override
